d46bf9f41e0ed58ed196afc7328504bae4c8b647
dharmatech <wayo.cavazos@gmail.com>
2023-11-26 18:05:50 -0800

    Generate path. lookback parameter.

--- treasury_gov_pandas.py
+++ treasury_gov_pandas.py
@@ -59,17 +59,88 @@ def download_records_after(url, date, page_size=10000):
 # start_date = '1900-01-01'
 # page_size = 10000
 
-def update_records(path, url, start_date='1900-01-01', page_size=10000):
+# def update_records(path, url, start_date='1900-01-01', page_size=10000):
     
+#     if os.path.isfile(path):
+#         print(f'Found {path}. Importing.')
+
+#         df = pd.read_pickle(path)
+
+#         # Get second most recent record_date
+#         recent_record_date = df['record_date'].unique()[-2]
+        
+#         print(f'Second most recent record_date: recent_record_date {recent_record_date}')
+        
+#         new_records = download_records_after(url, recent_record_date, page_size)
+        
+#         df = df[df['record_date'] <= recent_record_date]
+
+#         df = pd.concat([df, new_records], ignore_index=True)
+
+#         df.to_pickle(path)
+        
+#         return df
+
+#     else:
+           
+#         recent_record_date = start_date
+
+#         print(f'Using recent_record_date: {recent_record_date}')
+       
+#         df = download_records_after(url, recent_record_date, page_size)
+       
+#         df.to_pickle(path)
+
+#         return df
+
+
+# path = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts/operating_cash_balance'
+
+# url = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts/operating_cash_balance'
+
+
+# ls = path.split('/')
+
+# ls[3:]
+
+
+# string.replace('-'.join(ls[3:]), '_', '-')
+
+# '-'.join(ls[3:]).replace('_', '-')
+
+
+
+# '-'.join(   path.split('/')[3:]   ).replace('_', '-')
+
+
+# [i for i, s in enumerate(ls) if 'treasury.gov' in s]
+
+# index = next((i for i, s in enumerate(ls) if 'treasury.gov' in s), None)
+# if index is not None:
+#     result = ls[index+1:]
+# else:
+#     result = []
+
+
+# url = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/auctions_query'
+
+def update_records(url, start_date='1900-01-01', page_size=10000, path=None, lookback=2):
+    
+    if path is None:
+
+        ls = url.split('/')[3:]
+
+        path = '-'.join(ls).replace('_', '-') + '.pkl'
+
     if os.path.isfile(path):
         print(f'Found {path}. Importing.')
 
         df = pd.read_pickle(path)
 
         # Get second most recent record_date
-        recent_record_date = df['record_date'].unique()[-2]
+        recent_record_date = df['record_date'].unique()[-lookback]
         
-        print(f'Second most recent record_date: recent_record_date {recent_record_date}')
+        print(f'recent_record_date: {recent_record_date} lookback: {lookback}')
         
         new_records = download_records_after(url, recent_record_date, page_size)
         
