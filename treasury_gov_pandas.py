f66883e94300c19e1233a02b68b9c93ee0703cc9
dharmatech <wayo.cavazos@gmail.com>
2024-05-05 15:00:28 -0700

    Update

--- treasury_gov_pandas.py
+++ treasury_gov_pandas.py
@@ -49,88 +49,39 @@ def download_records_after(url, date, page_size=10000):
 
     return pd.DataFrame(data)
 # ----------------------------------------------------------------------
-
-# url = https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts/deposits_withdrawals_operating_cash
-
-# path = 'deposits_withdrawals_operating_cash.pkl'
-
-# path = 'data_act_compliance.pkl'
-# url  = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v2/debt/tror/data_act_compliance'
-# start_date = '1900-01-01'
-# page_size = 10000
-
-# def update_records(path, url, start_date='1900-01-01', page_size=10000):
+def url_to_path(url):
+        
+        ls = url.split('/')[3:]
     
-#     if os.path.isfile(path):
-#         print(f'Found {path}. Importing.')
-
-#         df = pd.read_pickle(path)
-
-#         # Get second most recent record_date
-#         recent_record_date = df['record_date'].unique()[-2]
+        path = '-'.join(ls).replace('_', '-') + '.pkl'
         
-#         print(f'Second most recent record_date: recent_record_date {recent_record_date}')
+        return path
+# ----------------------------------------------------------------------
+# def load_records(url, start_date='1900-01-01', page_size=10000, path=None):
         
-#         new_records = download_records_after(url, recent_record_date, page_size)
-        
-#         df = df[df['record_date'] <= recent_record_date]
-
-#         df = pd.concat([df, new_records], ignore_index=True)
-
-#         df.to_pickle(path)
-        
-#         return df
-
-#     else:
-           
-#         recent_record_date = start_date
-
-#         print(f'Using recent_record_date: {recent_record_date}')
-       
-#         df = download_records_after(url, recent_record_date, page_size)
-       
-#         df.to_pickle(path)
-
-#         return df
-
-
-# path = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts/operating_cash_balance'
-
-# url = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/dts/operating_cash_balance'
-
-
-# ls = path.split('/')
-
-# ls[3:]
-
-
-# string.replace('-'.join(ls[3:]), '_', '-')
-
-# '-'.join(ls[3:]).replace('_', '-')
-
-
-
-# '-'.join(   path.split('/')[3:]   ).replace('_', '-')
-
-
-# [i for i, s in enumerate(ls) if 'treasury.gov' in s]
-
-# index = next((i for i, s in enumerate(ls) if 'treasury.gov' in s), None)
-# if index is not None:
-#     result = ls[index+1:]
-# else:
-#     result = []
-
-
-# url = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/auctions_query'
-
+#         if path is None:
+#             path = url_to_path(url)
+    
+#         if os.path.isfile(path):
+#             print(f'Found {path}. Importing.')
+    
+#             df = pd.read_pickle(path)
+            
+#             return df
+    
+#         else:
+#             print(f'No {path} found. Downloading.')
+    
+#             df = download_records_after(url, start_date, page_size)
+            
+#             df.to_pickle(path)
+    
+#             return df
+# ----------------------------------------------------------------------
 def update_records(url, start_date='1900-01-01', page_size=10000, path=None, lookback=2):
     
     if path is None:
-
-        ls = url.split('/')[3:]
-
-        path = '-'.join(ls).replace('_', '-') + '.pkl'
+        path = url_to_path(url)
 
     if os.path.isfile(path):
         print(f'Found {path}. Importing.')
@@ -163,3 +114,46 @@ def update_records(url, start_date='1900-01-01', page_size=10000, path=None, loo
         df.to_pickle(path)
 
         return df
+
+
+def load_records(url, start_date='1900-01-01', page_size=10000, path=None, lookback=2, update=False):
+    
+    if path is None:
+        path = url_to_path(url)
+
+    if os.path.isfile(path):
+        print(f'Found {path}. Importing.')
+
+        df = pd.read_pickle(path)
+
+        if update == False:
+            return df
+
+        # Get second most recent record_date
+        recent_record_date = df['record_date'].unique()[-lookback]
+        
+        print(f'recent_record_date: {recent_record_date} lookback: {lookback}')
+        
+        new_records = download_records_after(url, recent_record_date, page_size)
+        
+        df = df[df['record_date'] <= recent_record_date]
+
+        df = pd.concat([df, new_records], ignore_index=True)
+
+        df.to_pickle(path)
+        
+        return df
+        
+    else:  
+        recent_record_date = start_date
+
+        print(f'Using recent_record_date: {recent_record_date}')
+       
+        df = download_records_after(url, recent_record_date, page_size)
+       
+        df.to_pickle(path)
+
+        return df
+
+
+ 
